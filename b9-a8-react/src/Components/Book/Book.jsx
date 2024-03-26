afb849e2d84439257ece6be479379936c2638521
Ahnabu <horairaabu013025@gmail.com>
2024-03-26 13:30:38 +0600

    books section finished

---
+++ b9-a8-react/src/Components/Book/Book.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    
+} from "@material-tailwind/react";
+import { FaRegStar } from "react-icons/fa";
+
+const Book = ({ book }) => {
+    const { bookName, author, image, rating, category, tags ,}= book
+    return (
+        <div >
+            
+            <Card className="mt-6 w-96">
+                <CardHeader color="blue-gray" className="relative h-[380px] object-center object-cover">
+                    <img
+                        src={image}
+                        alt="card-image"
+                    />
+                </CardHeader>
+                <div className="flex justify-around">
+                    <h1 className="text-[#23BE0A] border border-[#23BE0A] rounded-xl p-1 ml-3 text-center mt-5">{ tags[0]}</h1>
+                    <h1 className="text-[#23BE0A] border border-[#23BE0A] rounded-xl p-1 ml-3 text-center mt-5">{ tags[1]}</h1>
+                    
+                </div>
+                <CardBody>
+                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                        {bookName}
+                    </Typography>
+                    <Typography>
+                       By:{author}
+                    </Typography>
+                </CardBody>
+                <hr className="border border-dashed" />
+                <CardFooter className="pt-0">
+                    <div className=" flex justify-between" >
+                        <h1>{category}</h1>
+                        <h1 className=" flex gap-2">{rating} <FaRegStar className="mt-[3px]"/> </h1>
+                    </div>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+};
+
+export default Book;
--- b9-a8-react/src/Components/Books/Books.jsx
+++ b9-a8-react/src/Components/Books/Books.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 
 const Books = () => {
+    const [books, setBooks] = useState([])
+    useEffect(() => {
+             fetch('/books.json')
+            .then(response => response.json())
+            .then(data => setBooks(data))
+    },[])
+    console.log(books);
+    
     return (
+        
         <div>
+            <h1 className="text-5xl text-center text-black">Books</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+                {
+                    books.map(book => <Book key={book.bookId} book={book} ></Book>)
+                }
+            </div>
+            
             
         </div>
     );
